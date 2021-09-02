9e75f15c815cdd8ad971436de4f180f6224ee43e
github username <github email address>
2021-09-02 13:07:35 -0500

    commit tree b46cbf588bba03bfe01866d492d9a39654174561

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+    const element = document.getElementById('text')
+    element.textContent = 'This is really cool!'
+  });
+
+  
+
+
+
